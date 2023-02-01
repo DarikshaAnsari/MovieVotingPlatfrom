ee160ee203f142d16759868010bce7827521308d
Vanshika <vanshikav928@gmail.com>
2023-02-01 13:35:29 +0530

    vanshika

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <section id="header">
-      <div className="navbar">
-        <ul>
+      <div>
+        <ul id="navbar">
           <li>Home</li>
           <li>
             <Link className="btn bg-white text-success mx-1" to="/Login">
-              Login
+              {" "}
+              Login{" "}
             </Link>
           </li>
           <li>
             {" "}
             <Link className="btn bg-white text-success mx-1" to="/CreatUser">
+              {" "}
               SignUp
             </Link>
           </li>
