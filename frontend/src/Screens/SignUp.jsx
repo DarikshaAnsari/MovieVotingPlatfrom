6d4d11a4c3c0bb834e9f0804ee4ea0b94dec20f7
Vanshika <vanshikav928@gmail.com>
2023-02-01 22:07:43 +0530

    vanshika

--- frontend/src/Screens/SignUp.jsx
+++ frontend/src/Screens/SignUp.jsx
@@ -1,23 +1,54 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 export default function SignUp() {
+ /* const [credentials, setcredentials] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:3000/api/createuser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: credentials.name,
+        email: credentials.email,
+        password: credentials.password,
+       
+      }),
+    });
+    const json = await response.json();
+    console.log(json);
+
+    if (!json.success) {
+      alert("Enter valid Credentials");
+    }
+  };
+  const onChange = (event) => {
+    setcredentials({ ...credentials, [event.target.name]: event.target.value });
+  };*/
   return (
     <>
     <div className="loginPage">
     <div className="form-box">
       <h1>SignUp</h1>
-      <form>
+      <form  /*onSubmit={handleSubmit}*/>
        <div className="input-group">
        <div className="input-field">
-          <input type="text" placeholder="Name" />
+          <input type="text" placeholder="Name" /*value={credentials.name}
+              onChange={onChange}*/ />
          </div>
          <div className="input-field">
-          <input type="email" placeholder="Email" />
+          <input type="email" placeholder="Email" /* value={credentials.email}
+              onChange={onChange}*//>
          </div>
          <div className="input-field">
-          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Password" /*value={credentials.password}
+              onChange={onChange}*/ />
          </div>
          <div className="btn-field">
          <button type="submit" > Submit</button>
