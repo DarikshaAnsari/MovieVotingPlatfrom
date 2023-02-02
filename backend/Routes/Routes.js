2f7102c0c165e6310c0fb80ac58acdd8a02035e2
DarikshaAnsari <100822529+DarikshaAnsari@users.noreply.github.com>
2023-02-02 17:47:22 +0530

    Dariksha

--- backend/Routes/Routes.js
+++ backend/Routes/Routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const ser = require("../Models/UserInfo");
 const { body, validationResult } = require("express-validator");
 const {createUser,loginUser} = require('../Controllers/controller');
-router.post("/createuser",  [body("email").isEmail(),body("name").isLength({ min: 5 }),body("password", "Incorrect password").isLength({ min: 5 })],createUser);
+router.post("/createuser",  [body("email").isEmail(),body("name").isLength({ min:2 }),body("password", "Incorrect password").isLength({ min: 5 })],createUser);
 router.post("/loginuser",[body("email").isEmail(),body("password", "Incorrect password").isLength({ min: 5 })],loginUser)
 module.exports=router;
--- frontend/src/Screens/Login.jsx
+++ frontend/src/Screens/Login.jsx
@@ -1,19 +1,49 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function Login() {
+  const [credentials, setcredentials] = useState({ email: "", password: "" });
+  let navigate=useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:4000/api/loginuser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: credentials.email,
+        password: credentials.password,
+      }),
+    });
+    const json = await response.json();
+    console.log(json);
+    if (!json.success) {
+      alert("Enter valid credentials");
+    }
+    if (json.success) {
+      localStorage.setItem("userEmail",credentials.email);
+      localStorage.setItem("authToken",json.authToken);
+      console.log(localStorage.getItem("authToken"))
+      navigate("/");
+    }
+  };
+  const onChange = (event) => {
+    setcredentials({ ...credentials, [event.target.name]: event.target.value });
+  };
   return (
   <>
   <div className="loginPage">
     <div className="form-box">
       <h1>LogIn</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
        <div className="input-group">
          <div className="input-field">
-          <input type="email" placeholder="Email" />
+          <input type="email" placeholder="Email" name="email" onChange={onChange} value={credentials.email}/>
          </div>
          <div className="input-field">
-          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Password" name="password" onChange={onChange} value={credentials.password} />
          </div>
          <div className="btn-field">
          <button type="submit" > Submit</button>
